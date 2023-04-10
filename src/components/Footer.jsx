19110853d27ad14256869adfd9685b8b2e7b6a63
Rodrigo Bautista <rodrigobautistar2000@gmail.com>
2023-04-09 18:09:40 -0600

    deployment13

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
       <footer className="py-10">
           <p className="text-center font-bold">APV - Administrador de Pacientes de {''}
-              <span className="text-indigo-600">Veterinaria</span>
+              <span className="text-teal-600">Veterinaria</span>
            </p>
       </footer>
   )
