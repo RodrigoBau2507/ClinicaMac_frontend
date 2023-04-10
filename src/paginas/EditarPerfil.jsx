5eb13c27d569cae6e8637bf72d9451970dd6f772
Rodrigo Bautista <rodrigobautistar2000@gmail.com>
2023-04-09 18:22:18 -0600

    deployment13

--- src/paginas/EditarPerfil.jsx
+++ src/paginas/EditarPerfil.jsx
@@ -8,7 +8,7 @@ const EditarPerfil = () => {
 
     const { auth, actualizarPerfil } = useAuth()
     const [ perfil, setPerfil] = useState({})
-    const [alerta, setAlerta] = useState({})
+    const [alerta, setAlerta] = useState(false)
 
     useEffect( () => {
         setPerfil(auth)
@@ -22,11 +22,17 @@ const EditarPerfil = () => {
                 msg: 'Email y Nombre son obligatorios',
                 error: true
             })
+            setTimeout(() => {
+                setAlerta(false)
+              }, 5000);
             return
         }
 
         const resultado = await actualizarPerfil(perfil)
         setAlerta(resultado)
+        setTimeout(() => {
+            setAlerta(false)
+          }, 5000);
     }
 
     const { msg } = alerta
@@ -38,11 +44,11 @@ const EditarPerfil = () => {
 
             <h2 className="font-black text-3xl text-center mt-10">Editar Perfil</h2>
             <p className="text-xl mt-5 mb-10 text-center">Modifica tu {''} 
-                <span className="text-indigo-600 font-bold">Información aquí</span> 
+                <span className="text-teal-600 font-bold">Información aquí</span> 
             </p>
 
             <div className="flex justify-center">
-                <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
+                <div className="w-full md:w-1/2 bg-slate-400 shadow rounded-lg p-5">
 
                     {msg && <Alerta alerta={alerta} />}
                     <form
@@ -107,7 +113,7 @@ const EditarPerfil = () => {
                         <input 
                             type="submit"
                             value="Guardar Cambios"
-                            className="bg-indigo-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5"
+                            className="bg-teal-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5"
                         />
                     </form>
                 </div>
